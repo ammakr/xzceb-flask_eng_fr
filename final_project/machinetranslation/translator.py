2a55489db8d0d02c5e2ac9018cd8ac342a5c9615
ammakr <aakrambhutta@gmail.com>
2022-08-28 14:12:59 +0000

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -3,41 +3,45 @@ from dotenv import load_dotenv
 from ibm_watson import LanguageTranslatorV3, ApiException
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
+
 load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-authenticator = IAMAuthenticator('{apikey}')
+authenticator = IAMAuthenticator(f'{apikey}')
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('{url}')
+language_translator.set_service_url(f'{url}')
 
-
-def englishToFrench(englishText: str):
+def english_to_french(english_text: str):
+    if english_text is None:
+        return
     try:
         translated_text = language_translator.translate(
-            text=englishText,
+            text=english_text,
             model_id='en-fr').get_result()
-        frenchText = translated_text['translations'][0]['translation']
+        french_text = translated_text['translations'][0]['translation']
     except ApiException as ex:
-        frenchText = ''
-        print("Method failed with status code " + str(ex.code) + ": " + ex.message
+        french_text = ''
+        print("Method failed with status code " + str(ex.code) + ": " + ex.message)
 
-    return frenchText
+    return french_text
 
+def french_to_english(french_text: str):
+    if french_text is None:
+        return
 
-def frenchToEnglish(frenchText: str):
     try:
         translated_text = language_translator.translate(
-            text=frenchText,
+            text=french_text,
             model_id='fr-en').get_result()
-        englishText = translated_text["translations"][0]["translation"]
+        english_text = translated_text["translations"][0]["translation"]
     except ApiException as ex:
-        englishText = ''
+        english_text = ''
         print("Method failed with status code " + str(ex.code) + ": " + ex.message)
     
-    return englishText
+    return english_text
